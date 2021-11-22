1f52a20ddcfd5455941fcfe52bcdc3719d638556
ZHQ <herman.zheng@aya.yale.edu>
2021-11-22 16:05:13 -0500

    added customized token claims

--- todo_drf/api/urls.py
+++ todo_drf/api/urls.py
@@ -4,8 +4,8 @@ from django.urls.conf import include
 from . import views
 
 # Simple JWT
+from .views import MyTokenObtainPairView
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
 
@@ -16,7 +16,7 @@ urlpatterns = [
     path('task-create/', views.taskCreate, name='task-create'),
     path('task-update/<str:pk>/', views.taskUpdate, name='task-update'),
     path('task-delete/<str:pk>/', views.taskDelete, name='task-delete'),
-
-    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # Simple JWT
+    path('token/', MyTokenObtainPairView .as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- todo_drf/api/views.py
+++ todo_drf/api/views.py
@@ -1,13 +1,33 @@
-from django.http.response import JsonResponse
+# from django.http.response import JsonResponse
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework import serializers
+# from django.http import JsonResponse
 
+# REST Framework
+from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from api.models import Task
 from .serializers import TaskSerializer
+
+# Simple JWT
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 @api_view(['GET'])
 def apiOverview(request):
