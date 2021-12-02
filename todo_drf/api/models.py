5656fa5528f41fa51b50a3e6280f6cb46a6a2953
ZHQ <herman.zheng@aya.yale.edu>
2021-12-02 17:49:02 -0500

    created one-to-many relationship between User and Task models

--- todo_drf/api/models.py
+++ todo_drf/api/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-from django.db.models.expressions import F
+from django.contrib.auth.models import User
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
+
 class Task(models.Model):
+  user = models.ForeignKey(User, on_delete=CASCADE, default='')
   title = models.CharField(max_length=200)
   completed = models.BooleanField(blank=True, default=False, null=True)
 
