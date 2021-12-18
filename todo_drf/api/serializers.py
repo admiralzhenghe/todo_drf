6dff2854c85c200d9f2a1218930a49de98f92849
ZHQ <herman.zheng@aya.yale.edu>
2021-12-18 18:40:02 -0500

    updated imports

--- todo_drf/api/serializers.py
+++ todo_drf/api/serializers.py
@@ -1,5 +1,9 @@
-from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
+# Models
 from . models import Task
+from django.contrib.auth.models import User
+# Rest Framework
+from rest_framework import serializers
 
 # Serializers return a Model object in a JSON reponse
 class TaskSerializer(serializers.ModelSerializer):
